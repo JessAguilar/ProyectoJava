5e7c4889d82d3934b937017d8c288c37e39bdbf3
xxjuanandresxx <j.andrescastillo.711@gmail.com>
2018-08-24 16:37:15 -0400

    Segundo Commit

--- src/javaproyectone/JavaProyectOne.java
+++ src/javaproyectone/JavaProyectOne.java
@@ -15,7 +15,7 @@ public class JavaProyectOne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x;
     }
     
 }
